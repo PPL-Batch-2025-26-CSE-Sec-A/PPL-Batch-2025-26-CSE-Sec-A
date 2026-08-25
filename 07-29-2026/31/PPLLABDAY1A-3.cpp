13277f0fda3807ea1b10c484fa9d2a02c510525e
deyarpan4484-lang <deyarpan4484@gmail.com>
2026-08-25 10:22:52 +0530

    Add files via upload

---
+++ 07-29-2026/31/PPLLABDAY1A-3.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void countOddEven(int n)
+{
+    int i, num, odd = 0, even = 0;
+
+    for (i = 1; i <= n; i++)
+    {
+        scanf("%d", &num);
+
+        if (num % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even = %d\n", even);
+    printf("Odd = %d\n", odd);
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter number of integers: ");
+    scanf("%d", &n);
+
+    printf("Enter the numbers:\n");
+    countOddEven(n);
+
+    return 0;
+}
