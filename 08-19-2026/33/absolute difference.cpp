ad19375f43d045ff1972ebe363218462e449a199
satarupa133 <satarupamaiti06@gmail.com>
2026-08-25 19:50:42 +0530

    Add files via upload

---
+++ 08-19-2026/33/absolute difference.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter first number=";
+    cin >> n;
+
+    int difference = abs(n - 51);
+
+    if (n > 51)
+        difference = difference * 3;
+
+    cout << difference<<" is the difference.";
+
+    return 0;
+}
