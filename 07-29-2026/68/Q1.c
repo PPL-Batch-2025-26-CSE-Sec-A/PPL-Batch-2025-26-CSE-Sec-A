a704399db7b8041149be5a3429b215939aa4e7e5
Bignesh Kumar <bignesh1.kumar@stu.adamasuniversity.ac.in>
2026-08-25 09:58:45 +0530

    Add files via upload

---
+++ 07-29-2026/68/Q1.c
@@ -0,0 +1,34 @@
+//write a program in c to check for a prime number
+
+#include <stdio.h>
+
+int isPrime(int n)
+{
+    int i;
+
+    if (n <= 1)
+        return 0;
+
+    for (i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+            return 0;
+    }
+
+    return 1;
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (isPrime(n))
+        printf("%d is a Prime Number", n);
+    else
+        printf("%d is Not a Prime Number", n);
+
+    return 0;
+}
---
+++ 07-29-2026/68/Q2.C
@@ -0,0 +1,40 @@
+//write a program in c to check for a prime number within a range
+
+#include <stdio.h>
+
+int isPrime(int n)
+{
+    int i;
+
+    if (n <= 1)
+        return 0;
+
+    for (i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+            return 0;
+    }
+
+    return 1;
+}
+
+int main()
+{
+    int start, end, i;
+
+    printf("Enter starting number: ");
+    scanf("%d", &start);
+
+    printf("Enter ending number: ");
+    scanf("%d", &end);
+
+    printf("Prime numbers are:\n");
+
+    for (i = start; i <= end; i++)
+    {
+        if (isPrime(i))
+            printf("%d ", i);
+    }
+
+    return 0;
+}
