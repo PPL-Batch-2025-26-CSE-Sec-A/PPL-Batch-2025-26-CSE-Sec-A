db711b3153ff844fa01e83eb5c390352fa0715ad
Simran Mandal <simranmandalwork@gmail.com>
2026-08-25 11:10:54 +0530

    Add files via upload (#195)

---
+++ 08-19-2026/59/Add_2_number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,sum=0;
+	cout<<"Enter a number :";
+	cin>>a;
+	cout<<"Enter a number :";
+	cin>>b;
+	sum=a+b;
+	cout<<"Sum is :"<<sum;
+	return 0;
+}
