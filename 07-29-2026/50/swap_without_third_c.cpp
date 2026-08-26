017821ccde31546008b574f6761a16de2fe9653f
rosavineprotiva <protivabiswas5150@gmail.com>
2026-08-26 14:33:49 +0530

    Add files via upload (#264)

---
+++ 07-29-2026/50/swap_without_third_c.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	cout<<"Enter the first number:"<<endl;
+	cin>>a;
+	cout<<"Enter the second number:"<<endl;
+	cin>>b;
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	cout<<"After swaping"<<endl;
+	cout<<"a:"<<a;
+	cout<<"b:"<<b;
+	return 0;
+}
