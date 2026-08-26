0adc91a8456bb8e501dc2379b8d5f5ff5e76495a
Ishika Mahto <ishikamahto9@gmail.com>
2026-08-26 16:48:17 +0530

    Assignment 5

---
+++ 08-26-2026/19/program_9.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+class Employee
+{
+    float salary, allowance;
+public:
+    void input()
+    {
+        cout << "Enter salary: ";
+        cin >> salary;
+        cout << "Enter allowance: ";
+        cin >> allowance;
+    }
+    void display()
+    {
+        cout << "Salary = " << salary << endl;
+        cout << "Allowance = " << allowance << endl;
+    }
+};
+int main()
+{
+    Employee e[3];
+
+    for(int i = 0; i < 3; i++)
+    {
+        cout << "\nEnter details of Employee " << i + 1 << ":\n";
+        e[i].input();
+    }
+    cout << "\nEmployee Details:\n";
+    for(int i = 0; i < 3; i++)
+    {
+        cout << "\nEmployee " << i + 1 << ":\n";
+        e[i].display();
+    }
+    return 0;
+}
