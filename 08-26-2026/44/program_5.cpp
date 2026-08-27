1e3adb31a0fd1d8ef469c430d527e281a2fefd14
Reeshav choudhary <reeshavchoudhary86@gmail.com>
2026-08-27 16:50:36 +0530

    Add files via upload (#357)

---
+++ 08-26-2026/44/program_5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int start, end;
+    cout << "Enter starting number: ";
+    cin >> start;
+    cout << "Enter ending number: ";
+    cin >> end;
+    cout << "Perfect numbers are: ";
+    for(int n = start; n <= end; n++)
+    {
+        int sum = 0;
+        for(int i = 1; i < n; i++)
+        {
+            if(n % i == 0)
+                sum = sum + i;
+        }
+        if(sum == n)
+            cout << n << " ";
+    }
+    return 0;
+}
