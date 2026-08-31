3e7927b69b1613362700db8a89d7aa0e59040a57
Bignesh Kumar <bignesh1.kumar@stu.adamasuniversity.ac.in>
2026-08-31 17:14:45 +0530

    Add files via upload (#384)

---
+++ 08-26-2026/68/Q8.cpp
@@ -0,0 +1,46 @@
+//Create a class “Car” and accept the details of a car for its id and price. Display the details of 2 such car objects.
+
+#include <iostream>
+using namespace std;
+
+class Car
+{
+    int id;
+    float price;
+
+public:
+
+    void input()
+    {
+        cout << "Enter Car ID: ";
+        cin >> id;
+
+        cout << "Enter Car Price: ";
+        cin >> price;
+    }
+
+    void display()
+    {
+        cout << "Car ID = " << id << endl;
+        cout << "Car Price = " << price << endl;
+    }
+};
+
+int main()
+{
+    Car c1, c2;
+
+    cout << "Enter details of Car 1:" << endl;
+    c1.input();
+
+    cout << "\nEnter details of Car 2:" << endl;
+    c2.input();
+
+    cout << "\nDetails of Car 1:" << endl;
+    c1.display();
+
+    cout << "\nDetails of Car 2:" << endl;
+    c2.display();
+
+    return 0;
+}
