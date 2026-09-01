8e799d62d8f52b3a6db677a97a1e66d3539db17c
sahelipore27 <sahelipore69@gmail.com>
2026-09-01 10:38:52 +0530

    Add files via upload (#391)

---
+++ 08-26-2026/51/factorial.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int fact(int n){
+	int fact=1;
+	for(int i=0;i<n;i++){
+		fact=fact*i;
+	
+	
+	
+}
+return fact;
+}
+
+
+int main(){
+	int n;
+	printf("Enter the number:");
+	scanf("%d",&n);
+	
+int	fact();
+printf("factorial:",fact);
+return 0;	
+}
