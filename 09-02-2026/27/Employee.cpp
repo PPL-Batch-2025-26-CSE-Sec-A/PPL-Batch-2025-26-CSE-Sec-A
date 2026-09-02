c556293256722e020d605494db3f97889feb5691
Utsab Maiti <utsabmaiti2006@gmail.com>
2026-09-02 20:24:14 +0530

    Add files via upload (#483)

---
+++ 09-02-2026/27/Employee.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Employee {
+private:
+    int id;
+    string name;
+    double salary;
+    static int nextId;
+
+public:
+    Employee(string n, double s) : name(n), salary(s) {
+        id = nextId++;
+    }
+    void display() {
+        cout << "ID: " << id << " | Name: " << name << " | Salary: $" << salary << endl;
+    }
+};
+
+int Employee::nextId = 1001;
+
+int main() {
+    Employee e1("Arpan", 50000);
+    Employee e2("Arka", 60000);
+    e1.display();
+    e2.display();
+    return 0;
+}
