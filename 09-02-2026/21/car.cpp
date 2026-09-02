4d63b4e633d70d519c24f54319964a26484edb5a
manikmaity721635-dot <manikmaity721635@gmail.com>
2026-09-02 21:58:20 +0530

    Add files via upload (#487)

---
+++ 09-02-2026/21/car.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Car {
+private:
+    string model;
+    double price;
+    static int totalCars;
+
+public:
+    Car(string m, double p) : model(m), price(p) {
+        totalCars++;
+    }
+    
+    void display() {
+        cout << "Model: " << model << " | Price: $" << price << endl;
+    }
+
+    static void showTotal() {
+        cout << "Total cars created: " << totalCars << endl;
+    }
+};
+
+int Car::totalCars = 0;
+
+int main() {
+    Car c1("BMW", 25000);
+    Car c2("MAHINDRA", 22000);
+
+    c1.display();
+    c2.display();
+
+    Car::showTotal();
+    
+    return 0;
+}
