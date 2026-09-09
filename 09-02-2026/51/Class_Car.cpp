40cfef2998bae43acde490f1c43e0d148c7fae2c
sahelipore27 <sahelipore69@gmail.com>
2026-09-09 08:01:21 +0530

    Add files via upload (#571)

---
+++ 09-02-2026/51/Class_Car.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Car
+{
+    string model;
+    float price;
+
+    static int totalCars;
+
+public:
+
+    Car(string m, float p)
+    {
+        model = m;
+        price = p;
+        totalCars++;
+    }
+
+    void display()
+    {
+        cout << "Model: " << model << endl;
+        cout << "Price: " << price << endl;
+    }
+
+    static void showTotal()
+    {
+        cout << "Total Cars Created: " << totalCars << endl;
+    }
+};
+
+int Car::totalCars = 0;
+
+int main()
+{
+    Car c1("Toyota", 1500000);
+    Car c2("Honda", 1200000);
+    Car c3("BMW", 4500000);
+
+    c1.display();
+    cout << endl;
+
+    c2.display();
+    cout << endl;
+
+    c3.display();
+    cout << endl;
+
+    Car::showTotal();
+
+    return 0;
+}
---
+++ 09-02-2026/51/Store_employee's_basic_salary.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Employee
+{
+    float basicSalary;
+    float bonus;
+
+public:
+
+    Employee(float basic, float b)
+    {
+        basicSalary = basic;
+        bonus = b;
+    }
+
+    friend void calculateSalary(Employee e);
+};
+
+void calculateSalary(Employee e)
+{
+    float totalSalary;
+
+    totalSalary = e.basicSalary + e.bonus;
+
+    cout << "Basic Salary: " << e.basicSalary << endl;
+    cout << "Bonus: " << e.bonus << endl;
+    cout << "Total Salary: " << totalSalary << endl;
+}
+
+int main()
+{
+    Employee e(50000, 10000);
+
+    calculateSalary(e);
+
+    return 0;
+}
