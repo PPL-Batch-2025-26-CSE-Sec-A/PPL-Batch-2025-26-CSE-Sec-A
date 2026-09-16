bebb31c254458db1d85d835eec4f4e2aa615acfb
atifmd962 <atifmd962@gmail.com>
2026-09-16 16:14:01 +0530

    Add files via upload (#669)

---
+++ 09-16-2026/41/2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cout << "Enter three sides: ";
+    cin >> a >> b >> c;
+
+    // Check Pythagorean theorem
+    if (a * a + b * b == c * c ||
+        a * a + c * c == b * b ||
+        b * b + c * c == a * a) {
+        
+        cout << "It forms a right triangle.";
+    }
+    else {
+        cout << "It does not form a right triangle.";
+    }
+
+    return 0;
+}
