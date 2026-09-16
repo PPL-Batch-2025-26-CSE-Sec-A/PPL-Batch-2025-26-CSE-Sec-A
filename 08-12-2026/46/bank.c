f8a9d68a379e1f5b4f0bc352791b5cd4957fe28d
tuhinangshusingha <tuhinangshusingha@gmail.com>
2026-09-16 16:04:18 +0530

    Add files via upload (#665)

---
+++ 08-12-2026/46/bank.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+struct Book {
+    int bookid;
+    char bookname[100];
+    char author[100];
+    float price;
+    int year;
+    int pages;
+    char publisher[100];
+};
+
+int main() {
+    struct Book b;
+
+    printf("Enter Book ID: ");
+    scanf("%d", &b.bookid);
+
+    printf("Enter Book Name: ");
+    scanf(" %[^\n]", b.bookname);
+    printf("Enter Author Name: ");
+    scanf(" %[^\n]", b.author);
+
+    printf("Enter Price: ");
+    scanf("%f", &b.price);
+
+    printf("Enter Year of Publication: ");
+    scanf("%d", &b.year);
+
+    printf("Enter Number of Pages: ");
+    scanf("%d", &b.pages);
+
+    printf("Enter Publisher: ");
+    scanf(" %[^\n]", b.publisher);
+
+    printf("\n--- Book Details ---\n");
+    printf("Book ID: %d\n", b.bookid);
+    printf("Book Name: %s\n", b.bookname);
+    printf("Author: %s\n", b.author);
+    printf("Price: $%.2f\n", b.price);
+    printf("Publication Year: %d\n", b.year);
+    printf("Total Pages: %d\n", b.pages);
+    printf("Publisher: %s\n", b.publisher);
+
+    return 0;
+}
