52c35d3c07570a416296f3888847229c556b218c
SAYAN BAIRAGI <sayanbairagi123s@gmail.com>
2026-09-16 16:36:27 +0530

    Add files via upload

---
+++ 09-16-2026/25/Length_Conversion.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+float cm;
+cout<<"Enter length in centimeters: ";
+cin>>cm;
+cout<<"Meters: "<<cm/100<<endl;
+cout<<"Kilometers: "<<cm/100000;
+return 0;
+}
