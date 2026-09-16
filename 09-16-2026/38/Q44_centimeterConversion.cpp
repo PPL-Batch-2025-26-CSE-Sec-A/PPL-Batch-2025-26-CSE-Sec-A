89f5194f80e7ae52836959bcb3a31f7671d44d16
Jasmin Singh <jasminsingh.kolkata@gmail.com>
2026-09-17 00:35:49 +0530

    Enhance centimeter conversion to include kilometers
    Added conversion from centimeters to kilometers in the program.

--- 09-16-2026/38/Q44_centimeterConversion.cpp
+++ 09-16-2026/38/Q44_centimeterConversion.cpp
@@ -4,6 +4,7 @@ int main(){
     float a;
     cout<<"Enter a value (centimeter): ";
     cin>>a;
-    cout<<"The meter value is: "<<a/100<<" meter(s)";
+    cout<<"The meter value is: "<<a/100<<" meter(s)"<<endl;
+    cout<<"The kilometer value is: "<<a/100000<<" kilometer(s)";
     return 0;
 }
