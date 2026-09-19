f2acca18764d424dcf2229f09d634c5a7f1ddbde
Prithwis0404 <dasprithwis04@gmail.com>
2026-09-19 14:19:25 +0530

    Add files via upload

---
+++ 09-09-2026/21/student class.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+
+private:
+  int roll;
+  string name;
+  float marks;
+
+public:
+  
+   Student(int r, string n, float m) {
+    roll = r;
+    name = n;
+    marks = m;
+   }
+  
+   Student(const Student &s) {
+    roll  = s.roll;
+    name = s.name;
+    marks = s.marks;
+   }
+
+   void display() {
+    cout << "Roll: " << roll << endl;
+    cout << "Name: " << name << endl;
+    cout << "Marks: " << marks << endl;
+
+   }
+};
+
+int main(){
+    Student s1(27, "Utsab", 85.5);
+    cout << "Original Student:" << endl;
+    s1.display();
+
+    Student s2 = s1;
+    cout << "\nCopied Student:" << endl;
+    s2.display();
+
+    return 0;
+
+}
