63410d8a186b449a543dd0c545abde695bd047fa
Sibam Pradhan <sp7309143@gmail.com>
2026-09-20 20:52:41 +0530

    Add files via upload

---
+++ 09-16-2026/30/Q44_centimeterConversion.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a;
+    cout<<"Enter a value (centimeter): ";
+    cin>>a;
+    cout<<"The meter value is: "<<a/100<<" meter(s)"<<endl;
+    cout<<"The kilometer value is: "<<a/100000<<" kilometer(s)";
+    return 0;
+}
