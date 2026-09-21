32783cc49440d743553e6db5215e45307f1f4910
quantum-nil <quantumnilavra@gmail.com>
2026-09-21 17:50:07 +0530

    Add files via upload (#754)

---
+++ 09-16-2026/23/Descending_Order.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a[7];
+cout<<"Enter 7 numbers: ";
+for(int i=0;i<7;i++)cin>>a[i];
+for(int i=0;i<7;i++)
+for(int j=i+1;j<7;j++)
+if(a[i]<a[j])swap(a[i],a[j]);
+cout<<"Descending order: ";
+for(int i=0;i<7;i++)cout<<a[i]<<" ";
+return 0;
+}
