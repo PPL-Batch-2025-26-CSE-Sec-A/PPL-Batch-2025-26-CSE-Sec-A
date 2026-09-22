f47cf04aa597110dac94172c236c482cab92b8a7
Jasmin Singh <jasminsingh.kolkata@gmail.com>
2026-09-22 23:23:39 +0530

    Refactor array insertion logic and increase size (#820)
    Updated the array size and modified insertion logic to shift elements.

--- 08-05-2026/38/Q7_insertElement.c
+++ 08-05-2026/38/Q7_insertElement.c
@@ -1,19 +1,22 @@
 #include<stdio.h>
 int main(){
-    int arr[20],size,n,insert,i;
+    int arr[50],size,n,insert,i;
     printf("Enter how many elements the array will have:");
     scanf("%d",&size);
     for(i=0;i<size;i++){
         printf("Enter %d element:",i);
         scanf("%d",&arr[i]);
     }
-    printf("Which index element you want change:");
+    printf("At which index you want to insert:");
     scanf("%d",&n);
     printf("Now enter the element you want to insert at index %d:",n);
     scanf("%d",&insert);
+    for(i=size-1;i>=n;i--){
+        arr[i+1]=arr[i];
+    }
     arr[n]=insert;
-    printf("The new array is: ");
-    for(i=0;i<size;i++){
+    printf("Array after insertion ");
+    for(i=0;i<=size;i++){
         printf("%d ",arr[i]);
     }
     return 0;
