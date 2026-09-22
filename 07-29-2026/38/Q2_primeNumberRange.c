eed9ae87116c3130067e0624ed442db8524ac2e2
Jasmin Singh <jasminsingh.kolkata@gmail.com>
2026-09-22 23:12:07 +0530

    Refactor prime number checking logic (#812)

--- 07-29-2026/38/Q2_primeNumberRange.c
+++ 07-29-2026/38/Q2_primeNumberRange.c
@@ -1,28 +1,29 @@
 #include<stdio.h>
 int primeNum(int n){
     int i;
-    for(i=2;i<=n/2;i++){
-        if(n%i==0){
-            return 0;
-        }else{
-            return 1;
-        }
+    if(n<2){
+        return 0;
     }
 
-}
-int main(){
-    int num,l,i;
-    printf("Enter the first number:");
-    scanf("%d",&num);
-    printf("Enter the last number:");
-    scanf("%d",&l);
-    printf("The prime numbers are:");
-    for(i=num;i<=l;i++){
-        if(primeNum(i)){
-            printf("%d ",i);
-        }else{
-            continue;
+    for(i=2;i*i<=n;i++){
+        if(n%i==0){
+            return 0;
         }
     }
+    return 1;
+}
+int main(){
+    int i,a,b;
+    printf("Enter number from which you want to check for prime numbers: ");
+    scanf("%d",&a);
+    printf("Enter number till which you want to check for prime numbers: ");
+    scanf("%d",&b);
+    printf("Prime numbers are: ");
+    for(i=a;i<=b;i++){
+        if(primeNum(i)){
+            printf("%d ",i);
+        }
+    }
+    printf("\n");
     return 0;
 }
