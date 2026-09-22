c150d7a5c37d41d31c609a6552d085c6d8f729df
quantum-nil <quantumnilavra@gmail.com>
2026-09-22 22:59:36 +0530

    Add C program for inserting element in array

---
+++ 08-05-2026/23/assignment1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, pos, element, i;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the array elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the position to insert (1 to %d): ", n + 1);
+    scanf("%d", &pos);
+
+    if(pos < 1 || pos > n + 1) {
+        printf("Invalid position!");
+        return 0;
+    }
+
+    printf("Enter the element to insert: ");
+    scanf("%d", &element);
+
+    // Shift elements to the right
+    for(i = n; i >= pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    // Insert element
+    arr[pos - 1] = element;
+    n++;
+
+    printf("Array after insertion:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
